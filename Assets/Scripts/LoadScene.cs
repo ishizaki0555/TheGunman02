6213625a802fb3952ab8012448a1033ce44302f2
ishizaki0555 <ishizaki0555@icloud.com>
2025-10-23 22:48:47 +0900

    カメラ遷移をコントローラーからできるように改良、そこからシーン遷移できるようにしました。

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    // PlayerPrefsに保存されているシーン名のシーンをAdditiveモードで読み込む
+    void Start()
+    {
+        SceneManager.LoadScene(PlayerPrefs.GetString("SelectedScene"), LoadSceneMode.Additive);
+    }
+}
---
+++ Assets/Scripts/Title/TitleCameraManager.cs
@@ -0,0 +1,104 @@
+using Unity.Cinemachine;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using System.Collections;
+
+public class TitleCameraManager : MonoBehaviour
+{
+    [SerializeField] private CinemachineCamera titleCinameCamera; // 一番最初に優先度を上げるシネマカメラ
+    [SerializeField] List<CinemachineCamera> cinemaCameras = new List<CinemachineCamera>(); // 選択画面で使うシネマカメラを管理するリスト
+    [SerializeField] List<string> SceneNames = new List<string>(); // 各カメラに対応するシーン名を管理するリスト
+    [SerializeField] CinemachineCamera currentCamera; // 現在選択されているカメラ
+    [SerializeField] private string currentSceneName; // 現在選択されているシーン名
+
+    private int currentCameraIndex = 0;
+    private bool isCameraSlected = false;
+    [SerializeField] private bool isExperiment;
+
+    private void Start()
+    {
+        titleCinameCamera.Priority.Value = 1;
+    }
+
+    /// <summary>
+    /// タイトル画面での決定アクションです。
+    /// </summary>
+    /// <param name="context">InputSystemからの入力値です</param>
+    public void OnSelect(InputAction.CallbackContext context)
+    {
+        // カメラが選択されていない状態で決定ボタンが押されたらカメラを選択状態にする
+        if (context.performed && !isCameraSlected)
+        {
+            isCameraSlected = true;
+            titleCinameCamera.Priority.Value = 0;
+            cinemaCameras[currentCameraIndex].Priority.Value = 1;
+            currentCamera = cinemaCameras[currentCameraIndex];
+            currentSceneName = SceneNames[currentCameraIndex];
+        }
+        // 何もないシーンに移り、記録されているシーン名のシーンからオブジェクトを読み込む
+        else if (context.performed && isCameraSlected && !isExperiment)
+        {
+            // カメラが選択された状態で決定ボタンが押されたら次のシーンへ
+            PlayerPrefs.SetString("SelectedScene", currentSceneName);
+            UnityEngine.SceneManagement.SceneManager.LoadScene("MainGame");
+        }
+        // 記録されているシーン名のシーンからオブジェクトを非同期で読み込む
+        else if (context.performed && isCameraSlected && isExperiment)
+        {
+            StartCoroutine(LoadYourAsyncScene());
+        }
+    }
+
+    /// <summary>
+    /// 次のカメラへの切り替えアクションです。
+    /// </summary>
+    /// <param name="context">InputSystemからの入力値です</param>
+    public void OnNextCamera(InputAction.CallbackContext context)
+    {
+        if(context.performed && isCameraSlected)
+        {
+            currentCameraIndex++;
+            currentCameraIndex = (int)Mathf.Repeat(currentCameraIndex, cinemaCameras.Count);
+            SelectCamera();
+        }
+    }
+
+    /// <summary>
+    /// 前のカメラへの切り替えアクションです。
+    /// </summary>
+    /// <param name="context">InputSystemからの入力値です</param>
+    public void OnPrevCamera(InputAction.CallbackContext context)
+    {
+        if(context.performed && isCameraSlected)
+        {
+            currentCameraIndex--;
+            currentCameraIndex = (int)Mathf.Repeat(currentCameraIndex, cinemaCameras.Count);
+            SelectCamera();
+        }
+    }
+
+    /// <summary>
+    /// 現在のカメラから次のカメラ、または前のカメラに切り替えます。
+    /// シーンの名前も対応して更新します。
+    /// </summary>
+    private void SelectCamera()
+    {
+        currentCamera.Priority.Value = 0;
+        currentCamera = cinemaCameras[currentCameraIndex];
+        currentCamera.Priority.Value = 1;
+        currentSceneName = SceneNames[currentCameraIndex];
+    }
+
+    /// <summary>
+    /// シーンを非同期で読み込みます。
+    /// </summary>
+    IEnumerator LoadYourAsyncScene()
+    {
+        AsyncOperation asyncLoad = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(currentSceneName);
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
